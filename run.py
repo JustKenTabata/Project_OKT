88ebe27e9d31f149c5af8932c19a42618970c3a0
JustKenTabata <kntabataba@icloud.com>
2025-11-04 16:19:33 -0600

    Read until the head argument of routes.py. I have to read routes.py and models.py next and done?

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from app import create_app, db         
-from app.models import Video  
+from app import create_app, db
 
 app = create_app()
 
